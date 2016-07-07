3cd8898f8341aa5974612f847f338389774b2cfd
Mauve3 <mauvemoonman@gmail.com>
2016-07-07 12:20:17 -0700

    seomthignm

--- starter-code/ListExample/app/src/main/java/com/genassembly/dotdashdot/listexample/SimpleAdapter.java
+++ starter-code/ListExample/app/src/main/java/com/genassembly/dotdashdot/listexample/SimpleAdapter.java
@@ -51,37 +51,15 @@ public class SimpleAdapter extends BaseAdapter {
         if (v == null) {
 
             v = inflater.inflate(R.layout.list_item, parent, false);
-            //v.setTag(R.id.numWords, v.findViewById(R.id.numWords));
-            //v.setTag(R.id.genre, v.findViewById(R.id.genre));
-            //v.setTag(R.id.numSpaces, v.findViewById(R.id.numSpaces));
         }
 
         words = (TextView) v.findViewById(R.id.numWords);
         genre = (TextView) v.findViewById(R.id.genre);
         spaces = (TextView) v.findViewById(R.id.numSpaces);
 
-        Log.d("words: " , "" + words);
-        Log.d("genre: " , "" + genre);
-        Log.d("spaces: " , "" + spaces);
-
-        Log.d("words info: " , "" + libs.get(position).getWords());
-        Log.d("genre info: " , "" + libs.get(position).getGenre());
-        Log.d("spaces info: ", "" + libs.get(position).getSpaces());
-
         words.setText(String.valueOf(libs.get(position).getWords()));
         genre.setText(String.valueOf(libs.get(position).getGenre()));
         spaces.setText(String.valueOf(libs.get(position).getSpaces()));
-
-        /*TextView textView = (TextView) rowView.findViewById(R.id.label);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        textView.setText(values[position]);
-        // change the icon for Windows and iPhone
-        String s = values[position];
-        if (s.startsWith("iPhone")) {
-            imageView.setImageResource(R.drawable.no);
-        } else {
-            imageView.setImageResource(R.drawable.ok);
-        }*/
 
         return v;
     }
